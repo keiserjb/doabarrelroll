a9a669e3c435998dcf3f56831d8c04f0158b8ac4
Capi Etheriel <barraponto@gmail.com>
2011-12-01 16:22:31 -0200

    Issue #1336164 by barraponto: do aileron and barrel rolls

--- doabarrelroll.js
+++ doabarrelroll.js
@@ -4,7 +4,15 @@
     attach: function(context, settings) {
       $('#search-form, #search-block-form', context).keyup(function(event){
         if ($(this).find('input.form-text').val().toLowerCase() == 'do a barrel roll') {
-          $('body').addClass('doabarrelroll');
+          if (settings.doabarrelroll > 0) {
+            $('body').addClass('doabarrelroll');
+          }
+          else {
+            $('body').addClass('doanaileronroll');
+          }
+        }
+        if ($(this).find('input.form-text').val().toLowerCase() == 'do an aileron roll') {
+          $('body').addClass('doanaileronroll');
         }
       });
     }, 
