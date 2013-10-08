2a48334f7645386e808a0cab0888ba9ee0641275
Capi Etheriel <barraponto@gmail.com>
2013-10-08 09:06:08 -0300

    D8CX: initial port to 8.x

--- doabarrelroll.js
+++ doabarrelroll.js
@@ -1,21 +1,21 @@
-(function ($) {
+(function ($, Drupal, settings) {
 
   Drupal.behaviors.doaBarrelRoll =  {
     attach: function(context, settings) {
       $('#search-form, #search-block-form', context).keyup(function(event){
-        if ($(this).find('input.form-text').val().toLowerCase() == 'do a barrel roll') {
-          if (settings.doabarrelroll > 0) {
+        if ($(this).find('input.form-search').val().toLowerCase() == 'do a barrel roll') {
+          if (settings.doabarrelroll.style === 'barrel') {
             $('body').addClass('doabarrelroll');
           }
           else {
             $('body').addClass('doanaileronroll');
           }
         }
-        if ($(this).find('input.form-text').val().toLowerCase() == 'do an aileron roll') {
+        if ($(this).find('input.form-search').val().toLowerCase() == 'do an aileron roll') {
           $('body').addClass('doanaileronroll');
         }
       });
-    }, 
+    },
   };
 
- }(jQuery));
+ })(jQuery, Drupal, drupalSettings);
